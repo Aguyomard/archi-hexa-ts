66c09f0542f09585898b7167ae415c928871ee1b
alec <alec.guyomard@calmedica.com>
2025-04-08 15:56:17 +0200

    FollowUserUseCase added

---
+++ src/__tests__/follow-user.spec.test.ts
@@ -0,0 +1,63 @@
+import {
+  FollowUserCommand,
+  FollowUserUseCase,
+} from '../application/usecases/follow-user.usecase'
+import { InMemoryFolloweeRepository } from '../infra/followee.inmemory.repository'
+
+describe('Feature: Following a user', () => {
+  let fixture: Fixture
+
+  beforeEach(() => {
+    fixture = createFixture()
+  })
+
+  test('Alice can follow Bob', async () => {
+    fixture.givenUserFollowees({
+      user: 'Alice',
+      followees: ['Charlie'],
+    })
+
+    await fixture.whenUserFollows({
+      user: 'Alice',
+      userToFollow: 'Bob',
+    })
+
+    await fixture.thenUserFolloweesAre({
+      user: 'Alice',
+      followees: ['Charlie', 'Bob'],
+    })
+  })
+})
+
+const createFixture = () => {
+  const followeeRepository = new InMemoryFolloweeRepository()
+  const followUserUseCase = new FollowUserUseCase(followeeRepository)
+
+  return {
+    givenUserFollowees({
+      user,
+      followees,
+    }: {
+      user: string
+      followees: string[]
+    }) {
+      followeeRepository.givenExistingFollowees(
+        followees.map((f) => ({ user, followee: f }))
+      )
+    },
+
+    async whenUserFollows(followCommand: FollowUserCommand) {
+      await followUserUseCase.handle(followCommand)
+    },
+
+    async thenUserFolloweesAre(userFollowees: {
+      user: string
+      followees: string[]
+    }) {
+      const followees = followeeRepository.getFolloweesOf(userFollowees.user)
+      expect(followees).toEqual(userFollowees.followees)
+    },
+  }
+}
+
+type Fixture = ReturnType<typeof createFixture>
---
+++ src/infra/followee.inmemory.repository.ts
@@ -0,0 +1,29 @@
+import {
+  Followee,
+  FolloweeRepository,
+} from '../application/secondaryPorts/followee.repository'
+
+export class InMemoryFolloweeRepository implements FolloweeRepository {
+  followeesByUser = new Map<string, string[]>()
+
+  async saveFollowee(followee: Followee): Promise<void> {
+    this.addFollowee(followee)
+    return Promise.resolve()
+  }
+
+  async givenExistingFollowees(followees: Followee[]): Promise<void> {
+    followees.forEach((f) => {
+      this.addFollowee(f)
+    })
+  }
+
+  private addFollowee(followee: Followee): void {
+    const existingFollowees = this.followeesByUser.get(followee.user) ?? []
+    existingFollowees.push(followee.followee)
+    this.followeesByUser.set(followee.user, existingFollowees)
+  }
+
+  getFolloweesOf(user: string): string[] {
+    return this.followeesByUser.get(user) ?? []
+  }
+}
